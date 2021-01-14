6bc93ce2e725d3d694ad6450028a31916df8d5ea
Josélio Samy <joseliobackup@gmail.com>
2021-01-14 09:05:33 -0300

    routers added

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { Server } = require('http');
+const app = express();
+
+const PORT = process.env.PORT || 8877;
+
+app.use(express.static("public"))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/test-html', (req, res)=>{
+    res.sendFile(__dirname + '/pages/test-html.html')
+})
+app.get('/test-css', (req, res)=>{
+    res.sendFile(__dirname + '/pages/test-css.html')
+})
+app.get('/test-js', (req, res)=>{
+    res.sendFile(__dirname + '/pages/test-js.html')
+})
+
+app.listen(PORT, ()=>{
+    console.log('escutando a porta:' + PORT)
+})
+
--- pages/footer.php
+++
@@ -1,4 +0,0 @@
-<footer></footer>
-<script src="https://kit.fontawesome.com/05552c5881.js" crossorigin="anonymous"></script>
-</body>
-</html>
