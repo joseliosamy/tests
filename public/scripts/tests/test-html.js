52769f3bc928a3da3c48c70a8395226969cdcdd1
Josélio Samy <joseliobackup@gmail.com>
2021-01-16 11:36:23 -0300

    bug fixed

--- public/scripts/tests/test-html.js
+++ public/scripts/tests/test-html.js
@@ -1,6 +1,6 @@
 function testHtml(){
     const textarea = document.querySelector('textarea[name="test_html"]');
-    document.querySelector('section script').innerHTML = textarea.value
+    document.write(textarea.value)
 }
 const btnHtml = document.querySelector('input[type=button]')
 btnHtml.addEventListener('click', testHtml);
